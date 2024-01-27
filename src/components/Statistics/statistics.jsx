98148f585e663b75c17b687042b1357baadf8622
Tasdeed <ayonaziz@gmail.com>
2024-01-26 22:24:59 -0500

    Ex 1.9

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -4,18 +4,23 @@ const Statistics = (props) => {
   const total = good + neutral + bad;
   const avg = total / 3;
   const positive = good / total;
-
+  //conditional render
+  //only show the values when there is a click
   return (
     <div>
       <h1>Statistics</h1>
-
-      <p>No feedback given</p>
-      <p>good : {good}</p>
-      <p>neutral : {neutral}</p>
-      <p>bad : {bad}</p>
-      <p>total :{total}</p>
-      <p>average :{avg}</p>
-      <p>positive :{positive} %</p>
+      {(good === 0) & (neutral === 0) & (bad === 0) ? (
+        <p>No feedback given</p>
+      ) : (
+        <>
+          <p>good : {good}</p>
+          <p>neutral : {neutral}</p>
+          <p>bad : {bad}</p>
+          <p>all :{total}</p>
+          <p>average :{avg}</p>
+          <p>positive :{positive} %</p>
+        </>
+      )}
     </div>
   );
 };
